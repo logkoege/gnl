316949f4126517dacec8ace13605223e1dc6ecd8
logkoege <togokoegele@gmail.com>
2024-07-05 21:46:54 +0200

    l

---
+++ main.c
@@ -0,0 +1,11 @@
+int	main(void)
+{
+	int	fd;
+
+	fd = open("tourpal.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+}
