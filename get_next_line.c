8388ca0046d1b5c2800795d4bed1391c3f849f65
logkoege <167982040+logkoege@users.noreply.github.com>
2024-07-06 13:32:16 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -64,8 +64,6 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	if (!s1 && !s2)
 		return (NULL);
-	if (!s1)
-		return (ft_strdup(s2));
 	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
