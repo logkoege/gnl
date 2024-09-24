8c65e644956c00f44b64444eef3aafeeae63d38f
logkoege <167982040+logkoege@users.noreply.github.com>
2024-09-24 23:02:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@ int	main(void)
 {
 	int	fd;
 
-	fd = open("tourpal.txt", O_RDONLY);
+	fd = open("texte.txt", O_RDONLY);
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
